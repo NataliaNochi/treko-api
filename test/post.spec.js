77afaeede16f663d5bb1dffd240fc4a043e75047
NataliaNochi <natalia.nochi@hotmail.com>
2019-12-27 22:35:07 +0000

    Update post.spec.js

--- test/post.spec.js
+++ test/post.spec.js
@@ -7,108 +7,106 @@ chai.use(chaiHttp);
 const app = require('../app');
 const request = chai.request.agent(app);
 const expect = chai.expect;
-const rabbit = chai.request("http://rabbitmq:15672")
+const rabbit = chai.request('http://rabbitmq:15672');
 
 describe('post', () => {
 
-    context('quando eu cadastro uma tarefa', () => {
-        let task = { title: 'Estudar Mongoose', owner: 'eu@papito.io', done: false }
+    context('Quando cadastro uma tarefa', () => {
+        let task = { title: 'Estudar Mongoose', owner: 'test@teste.com', done: false }
 
-        before(done => {
+        before((done) => {
+
             rabbit
-                .delete("/api/queues/%2F/tasksdev/contents")
+                .delete('/api/queues/%2F/tasksdev/contents')
                 .auth('guest', 'guest')
                 .end((err, res) => {
-                    expect(res).to.has.status(204);
+                    expect(res.status).to.eql(204);
                     done();
                 })
         })
 
-        it('entao deve retornar 200', (done) => {
+        it('Deve retornar 200', (done) => {
             request
                 .post('/task')
                 .send(task)
                 .end((err, res) => {
-                    expect(res).to.has.status(200)
-                    expect(res.body.data.title).to.be.an('string')
-                    expect(res.body.data.owner).to.be.an('string')
-                    expect(res.body.data.done).to.be.an('boolean')
-                    done()
+                    expect(res.status).to.eql(200);
+                    expect(res.body.data.title).to.be.an('string');
+                    expect(res.body.data.owner).to.be.an('string');
+                    expect(res.body.data.done).to.be.an('boolean');
+                    done();
                 })
         })
 
-        it("e deve enviar email", (done) => {
-            let payload = { vhost: "/", name: "tasksdev", truncate: "50000", ackmode: "ack_requeue_true", encoding: "auto", count: "1000" }
+        it('Deve enviar um email', (done) => {
+
+            let payload = { vhost: "/", name: "tasksdev", truncate: "50000", ackmode: "ack_requeue_true", encoding: "auto", count: "1" }
 
             rabbit
-                .post("/api/queues/%2f/tasksdev/get")
+                .post('/api/queues/%2F/tasksdev/get')
                 .auth('guest', 'guest')
                 .send(payload)
                 .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.body[0].payload).to.contain(`Tarefa ${task.title} criada com sucesso!`)
+                    expect(res.status).to.eql(200);
+                    expect(res.body[0].payload).to.contain(`Tarefa ${task.title} criada com sucesso!`);
                     done();
                 })
         })
     })
 
-    context('quando nao informo o titulo', () => {
-        let task = { title: '', owner: 'eu@papito.io', done: false }
+    context('Quando não informo o titulo', () => {
+        let task = { title: '', owner: 'test@teste.com', done: false }
 
-        it('entao deve retornar 400', (done) => {
+        it('Deve retornar 400', (done) => {
             request
                 .post('/task')
                 .send(task)
                 .end((err, res) => {
-                    expect(res).to.has.status(400)
-                    expect(res.body.errors.title.message).to.eql('Oops! Title is required.')
-                    done()
+                    expect(res.status).to.eql(400);
+                    expect(res.body.errors.title.message).to.eql('Oops! Title is required.');
+                    done();
                 })
         })
     })
 
-    context('quando nao informo o dono', () => {
-        let task = { title: 'Nova tarefa', owner: '', done: false }
+    context('Quando não informo o owner', () => {
+        let task = { title: 'Test', owner: '', done: false }
 
-        it('entao deve retornar 400', (done) => {
+        it('Deve retornar 400', (done) => {
             request
                 .post('/task')
                 .send(task)
                 .end((err, res) => {
-                    expect(res).to.has.status(400)
-                    expect(res.body.errors.owner.message).to.eql('Oops! Owner is required.')
-                    done()
+                    expect(res.status).to.eql(400);
+                    expect(res.body.errors.owner.message).to.eql('Oops! Owner is required.');
+                    done();
                 })
         })
+
     })
 
-    context('quando a tarefa já existe', () => {
-
-        let task = { title: 'Planejar viagem para a China', owner: 'eu@papito.io', done: false }
+    context('Quando a tarefa já existe', () => {
+        let task = { title: 'Planejar viagem para China', owner: 'test@teste.com', done: false }
 
         before((done) => {
             request
                 .post('/task')
                 .send(task)
                 .end((err, res) => {
-                    expect(res).to.has.status(200)
-                    done()
+                    expect(res.status).to.eql(200);
+                    done();
                 })
         })
 
-        it('deve retornar 409', (done) => {
+        it('Deve retornar 409', (done) => {
             request
                 .post('/task')
                 .send(task)
                 .end((err, res) => {
-                    expect(res).to.has.status(409)
-                    expect(res.body.errmsg).to.include('duplicate key')
-                    done()
+                    expect(res.status).to.eql(409);
+                    expect(res.body.errmsg).to.include('duplicate key');
+                    done();
                 })
         })
-
-
     })
-
-
 })
