4c9e871b233e2448f4e2d21bbf11e072f1babe62
NataliaNochi <natalia.nochi@hotmail.com>
2019-12-27 22:35:40 +0000

    Update put.spec.js

--- test/put.spec.js
+++ test/put.spec.js
@@ -1,6 +1,7 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import tasksModel from '../models/task'
+import { get } from 'mongoose';
 
 chai.use(chaiHttp);
 
@@ -9,49 +10,42 @@ const request = chai.request.agent(app);
 const expect = chai.expect;
 
 describe('put', () => {
-
-    context('quando eu altero uma tarefa', () => {
-
+    context('Quando altero uma tarefa', () => {
         let task = {
             _id: require('mongoose').Types.ObjectId(),
-            title: 'Comprar Fandagos',
-            owner: 'eu@papito.io',
+            title: 'Comprar fandangos',
+            owner: 'eu@test.com',
             done: false
         }
 
         before((done) => {
-            tasksModel.insertMany([task], (error, docs) => {
-                expect(error).to.be.null
-            });
+            tasksModel.insertMany([task])
             done()
         })
 
-        it('entao deve retornar 200', (done) => {
-            task.title = 'Comprar Baconzitos',
-            task.done = true
+        it('Então deve retornar 200', (done) => {
+            task.title = 'Comprar baconzitos',
+                task.done = true
+
             request
                 .put('/task/' + task._id)
                 .send(task)
                 .end((err, res) => {
-                    expect(res).to.have.status(200)
+                    expect(res.status).to.eql(200);
                     expect(res.body).to.eql({})
                     done()
                 })
         })
 
-        it('e deve retornar os dados atualizados', (done) => {
+        it('Deve retornar os dados atualizados', (done) => {
             request
                 .get('/task/' + task._id)
                 .end((err, res) => {
-                    expect(res).to.have.status(200)
+                    expect(res.status).to.eql(200);
                     expect(res.body.data.title).to.eql(task.title)
                     expect(res.body.data.done).to.be.true
                     done()
                 })
         })
-
-
     })
-
-
 })
